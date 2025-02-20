5227c2c32ac5ea190a4d7c4397f68126240d3be3
Zoey Bennett <zkb25@drexel.edu>
2025-02-20 14:41:43 -0500

    server stuff

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; ///change when you want for local
+const apiKey = process.env.VIRUSTOTAL_API_KEY;
+
+if (!apiKey) {
+  console.error('Missing VirusTotal API key! Check your .env file.');
+  process.exit(1);
+}
+
+console.log('VirusTotal API Key loaded successfully');
+
 
 
 app.use(cors());
@@ -13,12 +22,23 @@ app.use(express.json());
 
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage });
+app.use(express.static('public_html')); // Serves static files
+
 
 // VirusTotal URLs
 const FILE_SCAN_URL = 'https://www.virustotal.com/vtapi/v2/file/scan';
 const URL_SCAN_URL = 'https://www.virustotal.com/vtapi/v2/url/scan';
 const URL_REPORT_URL = 'https://www.virustotal.com/vtapi/v2/url/report';
 const FILE_REPORT_URL = 'https://www.virustotal.com/vtapi/v2/file/report';
+
+
+
+
+
+// Root route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public_html/index.html');
+});
 
 // Scan URL
 app.post('/scan-url', async (req, res) => {
